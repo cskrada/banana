08ccc941dbe25aabd538712fdf0e43ab264cad07
frada <frada@fradas-iMac.local>
2018-08-15 11:41:21 -0430

    se agrego codigo al provider clients

--- src/providers/data/clients.ts
+++ src/providers/data/clients.ts
@@ -1,17 +1,39 @@
-// importaciones de librerias 
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
-import 'rxjs/add/operator/map';
+import { Observable } from 'rxjs/Observable';
+import { map, catchError } from 'rxjs/operators';
 
 @Injectable()
 
 export class ClientsProvider {
 
+    private apiUrl = '';
 
-constructor(public http: Http) {
-
-	
+constructor(public http: HttpClient) {
 
 }
+    getClients(): Observable<string[]> {
+        return this.http.get(this.apiUrl).pipe(
+            map(this.extractData),
+            catchError(this.handleError)
+        );
+    }
+
+    private extractData(res: Response) {
+        let body = res;
+        return body || {};
+    }
+
+    private handleError (error: Response | any) {
+        let errMsg: string;
+            if (error instanceof Response) {
+                const err = error || '';
+                errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+            } else {
+                errMsg = error.message ? error.message : error.toString();
+            }
+        console.error(errMsg);
+        return Observable.throw(errMsg);
+        }
 
 }
