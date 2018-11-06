36252efeda5ce995bc232225b38e925814daec36
frada <frada@fradas-iMac.local>
2018-11-06 17:52:54 -0430

    archivo donde se almacenan las constantes de las rutas para el consumo de servicio

---
+++ src/const/const.ts
@@ -0,0 +1,8 @@
+export const constants = {
+    
+    apilogin : 'http://18.223.231.86/laravel-banana/public/api/login',
+    apiclients : 'http://18.223.231.86/laravel-banana/public/api/thirds/customers/'
+
+    // apilogin: 'http://192.168.1.3:8000/api/login',
+    // apiclient: 'http://192.168.1.3:8000/api/thirds/customers/'
+}
