6d354af1728c657e7666fc0e7e52f71bbf6502e4
frada <frada@fradas-iMac.local>
2018-11-06 11:51:32 -0430

    se agrego nueva palabra para la traduccion es/en y se importo en la pagina orders

--- src/pages/orders/orders.module.ts
+++ src/pages/orders/orders.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { OrdersPage } from './orders';
+import { TranslateModule } from '@ngx-translate/core';
+
 
 @NgModule({
   declarations: [
@@ -8,6 +10,7 @@ import { OrdersPage } from './orders';
   ],
   imports: [
     IonicPageModule.forChild(OrdersPage),
+    TranslateModule
   ],
 })
 export class OrdersPageModule {}
