97f74ddb8ef1329ee4985e8fc2077283207af9d0
frada <frada@fradas-iMac.local>
2018-11-06 11:48:32 -0430

    se agrego una nueva pagina que va a representar la vista especifica del producto/articulo

---
+++ src/pages/product/product.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { ProductPage } from './product';
+import { TranslateModule } from '@ngx-translate/core';
+
+
+@NgModule({
+  declarations: [
+    ProductPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(ProductPage),
+    TranslateModule
+  ],
+})
+export class ProductPageModule {}
