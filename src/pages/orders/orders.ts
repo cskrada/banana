698e66fac9cb1624bb43170ce75283f12cc1bdd2
frada <frada@fradas-iMac.local>
2018-11-06 11:56:27 -0430

    se agrego la funcion de abrir la pagina de product, en la primera tarjeta de la pagina de pedidos

--- src/pages/orders/orders.ts
+++ src/pages/orders/orders.ts
@@ -1,12 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the OrdersPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { ProductPage } from './../product/product';
 
 @IonicPage()
 @Component({
@@ -20,6 +14,10 @@ export class OrdersPage {
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad OrdersPage');
+  }
+
+  product(){
+    this.navCtrl.push(ProductPage);
   }
 
 }
