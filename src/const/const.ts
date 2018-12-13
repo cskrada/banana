dd678c7b29f4f0e2cc4d17cff2da1c3cf7fad826
cskrada <frada@overcom.net>
2018-12-13 16:50:14 -0430

    se agrego una constante para la creacion de prospecto

--- src/const/const.ts
+++ src/const/const.ts
@@ -1,7 +1,11 @@
 export const constants = {
     
     apilogin : 'http://18.223.231.86/laravel-banana/public/api/login',
-    apiclients : 'http://18.223.231.86/laravel-banana/public/api/thirds/customers/'
+    apiclients : 'http://18.223.231.86/laravel-banana/public/api/thirds/customers/',
+    apicountries : 'http://18.223.231.86/laravel-banana/public/api/location/countries',
+    apistates : 'http://18.223.231.86/laravel-banana/public/api/location/states',
+    apicities : 'http://18.223.231.86/laravel-banana/public/api/location/cities',
+    apipostclient: 'http://18.223.231.86/laravel-banana/public/api/thirds/prospect/create'
 
     // apilogin: 'http://192.168.1.3:8000/api/login',
     // apiclient: 'http://192.168.1.3:8000/api/thirds/customers/'
