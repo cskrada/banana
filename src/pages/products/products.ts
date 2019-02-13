3b124ec3aa78ffc092df83193623b4561ae31004
cskrada <frada@overcom.net>
2019-02-13 16:27:37 -0430

    vista de productos tipo cuadricula

--- src/pages/products/products.ts
+++ src/pages/products/products.ts
@@ -1,21 +1,50 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the ProductsPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @IonicPage()
 @Component({
   selector: 'page-products',
   templateUrl: 'products.html',
 })
 export class ProductsPage {
-
+  public products : any[] = [];
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+   this.items();
+
+  }
+  items(){
+    this.products=[
+      {
+        id : '1',
+        img : 'calabaza.jpg',
+        title : 'Lampara',
+        detail : 'Lorem ipsum dolor sit amet consectetur'
+      },
+      {
+        id : '2',
+        img : 'icon.png',
+        title : 'Ccama',
+        detail : 'Lorem ipsum dolor sit amet consectetur'
+      },
+      {
+        id : '3',
+        img : 'bananaApp.png',
+        title : 'Huawei',
+        detail : 'Lorem ipsum dolor sit amet consectetur'
+      },
+      {
+        id : '4',
+        img : 'icon.png',
+        title : 'Cuchuillos de plastico',
+        detail : 'Lorem ipsum dolor sit amet consectetur'
+      },
+      {
+        id : '5',
+        img : 'bananaApp.png',
+        title : 'Platos de acero',
+        detail : 'Lorem ipsum dolor sit amet consectetur'
+      }
+    ]
   }
 
   ionViewDidLoad() {
