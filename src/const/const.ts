6b618dbbc97eeedbb3c713dd681789f601ce556c
cskrada <frada@overcom.net>
2019-02-21 16:29:33 -0430

    se modifico la ruta de la api para el consumo de servicio

--- src/const/const.ts
+++ src/const/const.ts
@@ -5,7 +5,9 @@ export const constants = {
     apicountries : 'http://18.223.231.86/laravel-banana/public/api/location/countries',
     apistates : 'http://18.223.231.86/laravel-banana/public/api/location/states',
     apicities : 'http://18.223.231.86/laravel-banana/public/api/location/cities',
-    apipostclient: 'http://18.223.231.86/laravel-banana/public/api/thirds/prospect/create'
+    apipostclient: 'http://18.223.231.86/laravel-banana/public/api/thirds/prospect/create',
+
+    apiproducts: 'http://18.223.231.86/laravel-banana/public/api/products'
 
     // apilogin: 'http://192.168.1.3:8000/api/login',
     // apiclient: 'http://192.168.1.3:8000/api/thirds/customers/'
