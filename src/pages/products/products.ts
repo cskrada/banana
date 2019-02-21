f1e53de06cb848626e74f2edbae5a8cf7f9006b0
cskrada <frada@overcom.net>
2019-02-21 16:32:17 -0430

    se anadieron los datos dinamicos, consumo de servicio

--- src/pages/products/products.ts
+++ src/pages/products/products.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { constants } from './../../const/const';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { SeeproductPage } from './../seeproduct/seeproduct';
 
 @IonicPage()
 @Component({
@@ -9,49 +13,71 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class ProductsPage {
   view: string = "list";
   isAndroid: boolean = false;
-  public products : any[] = [];
+  public id : any ;
+  // public products : any[] = [];
+  public productos : any[] = [];
   
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-   this.items();
+  constructor(public navCtrl: NavController, public navParams: NavParams,public http: HttpClient) {
+  //  this.items();
 
   }
-  items(){
-    this.products=[
-      {
-        id : '1',
-        img : 'calabaza.jpg',
-        title : 'Lampara',
-        detail : 'Lorem ipsum dolor sit amet consectetur'
-      },
-      {
-        id : '2',
-        img : 'icon.png',
-        title : 'Cama',
-        detail : 'Lorem ipsum dolor sit amet consectetur'
-      },
-      {
-        id : '3',
-        img : 'bananaApp.png',
-        title : 'Huawei',
-        detail : 'Lorem ipsum dolor sit amet consectetur'
-      },
-      {
-        id : '4',
-        img : 'icon.png',
-        title : 'Cuchillos de plastico',
-        detail : 'Lorem ipsum dolor sit amet consectetur'
-      },
-      {
-        id : '5',
-        img : 'bananaApp.png',
-        title : 'Platos de acero',
-        detail : 'Lorem ipsum dolor sit amet consectetur'
-      }
-    ]
-  }
+  // items(){
+  //   this.products=[
+  //     {
+  //       id : '1',
+  //       img : 'calabaza.jpg',
+  //       title : 'Lampara',
+  //       detail : 'Lorem ipsum dolor sit amet consectetur'
+  //     },
+  //     {
+  //       id : '2',
+  //       img : 'icon.png',
+  //       title : 'Cama',
+  //       detail : 'Lorem ipsum dolor sit amet consectetur'
+  //     },
+  //     {
+  //       id : '3',
+  //       img : 'bananaApp.png',
+  //       title : 'Huawei',
+  //       detail : 'Lorem ipsum dolor sit amet consectetur'
+  //     },
+  //     {
+  //       id : '4',
+  //       img : 'icon.png',
+  //       title : 'Cuchillos de plastico',
+  //       detail : 'Lorem ipsum dolor sit amet consectetur'
+  //     },
+  //     {
+  //       id : '5',
+  //       img : 'bananaApp.png',
+  //       title : 'Platos de acero',
+  //       detail : 'Lorem ipsum dolor sit amet consectetur'
+  //     }
+  //   ]
+  // }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad ProductsPage');
+    this.getProducts();
   }
+
+  getProducts(){
+    return this.http.get(constants.apiproducts,
+      { headers: new HttpHeaders()
+        .set('authorization', 'http://localhost:4200')
+        .append('app', 'BananaApp')
+        .append('user', sessionStorage.getItem('user'))
+        .append('Access-Control-Allow-Origin', '*')
+        .append('token', sessionStorage.getItem('token'))
+      }).subscribe ( data=> {
+        this.productos = data['products'];
+        console.log('data products: ', this.productos);
+      }, error => {
+        console.log(error);
+    });
+  }
+
+  seeProduct(p){
+    this.navCtrl.push(SeeproductPage, p);
+  }  
 
 }
