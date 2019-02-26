280993ffcd357421827a77f48a869c760b7c3bac
cskrada <frada@overcom.net>
2019-02-26 17:33:59 -0430

    se modifico la logica de los metodos de archivo ts y se modifico la vista de ver producto

--- src/pages/seeproduct/seeproduct.ts
+++ src/pages/seeproduct/seeproduct.ts
@@ -1,5 +1,8 @@
+import { constants } from './../../const/const';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 
 @IonicPage()
 @Component({
@@ -8,19 +11,51 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class SeeproductPage {
 
+  category: any [] = [];
+  id_cat : any;
+  categories: any[] = [];
+  productDetails: any[] = [];
+  producto: any[] = [];
   product: any[] = [];
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  id: any;
+  constructor(public navCtrl: NavController, public navParams: NavParams, public http: HttpClient) {
     
     this.product = this.navParams.data;
-    console.log(this.product);
+    console.log('navparams de producto',this.product);
+    this.id = this.navParams.get('id');
+    this.id_cat = this.navParams.get('category_id');
+    console.log( 'id de categoria',this.id_cat);
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad SeeproductPage');
+    this.getProduct();
   }
 
   notificationSelect() {
     console.log('STP selected');
+  }
+
+  getProduct(){
+    return this.http.get(constants.apiseeproduct+this.id,
+      { headers: new HttpHeaders()
+        .set('authorization', 'http://localhost:4200')
+        .append('app', 'BananaApp')
+        .append('user', sessionStorage.getItem('user'))
+        .append('Access-Control-Allow-Origin', '*')
+        .append('token', sessionStorage.getItem('token'))
+      }).subscribe ( data=> {
+        this.producto = data['product'];
+        this.productDetails = data['product_details'];
+        this.categories = data['categories'];
+        for (let cat of this.categories){
+          if ( this.id_cat == cat.id){
+            this.category = cat.text;
+          }
+        }
+        console.log(this.category);
+      }, error => {
+        console.log(error);
+    });
   }
 
 }
