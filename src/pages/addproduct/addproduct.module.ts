a9780017e54c3ecd811321d5b3eda8fc3ed2d1eb
cskrada <frada@overcom.net>
2019-02-27 17:14:04 -0430

    traducciones

--- src/pages/addproduct/addproduct.module.ts
+++ src/pages/addproduct/addproduct.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { AddproductPage } from './addproduct';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [
@@ -8,6 +9,7 @@ import { AddproductPage } from './addproduct';
   ],
   imports: [
     IonicPageModule.forChild(AddproductPage),
+    TranslateModule
   ],
 })
 export class AddproductPageModule {}
