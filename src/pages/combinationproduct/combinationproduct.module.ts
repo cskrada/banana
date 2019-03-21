0a971e5687340ecc63270eaa4770bcbcff96628f
cskrada <frada@overcom.net>
2019-03-21 14:54:06 -0430

    agregado dos paginas, producto combinado y producto simple

---
+++ src/pages/combinationproduct/combinationproduct.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { CombinationproductPage } from './combinationproduct';
+
+@NgModule({
+  declarations: [
+    CombinationproductPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(CombinationproductPage),
+  ],
+})
+export class CombinationproductPageModule {}
---
+++ src/pages/simpleproduct/simpleproduct.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { SimpleproductPage } from './simpleproduct';
+
+@NgModule({
+  declarations: [
+    SimpleproductPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(SimpleproductPage),
+  ],
+})
+export class SimpleproductPageModule {}
