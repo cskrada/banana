3cec345f251eb83e23aceebef1a16f11cd413b51
cskrada <frada@overcom.net>
2019-04-17 15:42:24 -0430

    constantes agregadas para crear producto, consumir recursos

--- src/const/const.ts
+++ src/const/const.ts
@@ -11,7 +11,9 @@ export const constants = {
     apiseeproduct: 'http://18.223.231.86/laravel-banana/public/api/product/',
     apiproducts_details: 'http://18.223.231.86/laravel-banana/public/api/products/details',
     resources: 'http://18.223.231.86/laravel-banana/public/api/products/resources',
-    apipostproduct: 'http://18.223.231.86/laravel-banana/public/api/products/create'
+    apipostproduct: 'http://18.223.231.86/laravel-banana/public/api/products/create',
+
+    apiorganization: 'http://18.223.231.86/laravel-banana/public/api/organizations/byUser'
 
     // apilogin: 'http://192.168.1.3:8000/api/login',
     // apiclient: 'http://192.168.1.3:8000/api/thirds/customers/'
