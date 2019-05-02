d9f05d2e28e8bbc8b339c4e7e9a6352287b7ad5e
cskrada <frada@overcom.net>
2019-05-02 16:26:45 -0430

    producto combinado en proceso

--- src/pages/combinationproduct/combinationproduct.ts
+++ src/pages/combinationproduct/combinationproduct.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams,ToastController, ViewController } from 'ionic-angular';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { constants } from './../../const/const';
+import { ProductsPage } from './../products/products';
+import { TranslateService } from '@ngx-translate/core';
+
 
 @IonicPage()
 @Component({
@@ -10,33 +13,87 @@ import { constants } from './../../const/const';
   templateUrl: 'combinationproduct.html',
 })
 export class CombinationproductPage {
+  
   myForm: FormGroup;
+  
+  public product : any []=[];
   public resources : any []=[];
   public manufacturers : any []=[];
   public categories : any []=[];
   public taxes : any []=[];
   public units : any []=[];
   public conditions : any []=[];
+  public attributes : any []=[];
   public manufacture : any = 0;
   public category : any = 0;
   public unit : any = 0;
   public tax : any = 0;
   public condition : any = 0;
+  public is_salable : boolean=false;
+  public is_purchasable : boolean=false;
+  public manufacture_id: any = null;
+  public category_id: any = null;
+  public unit_id: any = null;
+  public tax_id: any = null;
+  public condition_id: any = null;
+  public price_list_id: any = null;
+  public attribute_id: any = null;
+  public product_details : any []=[];
+  public attribute_details: any = null;
+  public product_d: any = null;
+  
+  public items: any;
+  public session_org: any;
 
   constructor(public navCtrl: NavController,
               public navParams: NavParams,
               public formBuilder: FormBuilder,
-              public http: HttpClient) {
-    
+              public http: HttpClient,
+              public viewCtrl: ViewController,
+              public toastCtrl: ToastController,
+              public translateService: TranslateService,) {
+    this.getResource();
+
+    this.session_org=sessionStorage.getItem('organization_id');
+
     this.myForm = this.formBuilder.group({
       name: ['', Validators.required],
+      reference: ['', Validators.required],
       description: ['', Validators.required],
-    })
+      type: [''],
+      is_salable: [''],
+      is_purchasable: [''],
+      manufacture_id: [''],
+      category_id: [''],
+      unit_id: [''],
+      tax_id: [''],
+      is_combination: 0,
+      archived: 0,
+      organizations: [[this.session_org]], 
+      product_details: this.formBuilder.group({
+        reference: [''],
+        name: [''],
+        sku: [''],
+        ean13: [''],
+        upc: [''],
+        cost: [''],
+        sale_price: [''],
+        image: [''],
+        condition_id: [''],
+        price_list_id: [''],
+        attribute_details:  ['']
+        })
+      });
+
+      console.log(this.myForm);
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad CombinationproductPage');
-    this.getResource();
+    // console.log('ionViewDidLoad CombinationproductPage');
+  }
+
+  generate(){
+    console.log('generatee',this.product_d);
   }
 
   getResource(){
@@ -55,15 +112,67 @@ export class CombinationproductPage {
       this.taxes = data['taxes'];
       this.units = data['units'];
       this.conditions = data['conditions'];
-      // console.log('condiciones', this.conditions);
-      // console.log('data resources: ', this.resources );
-      // console.log('data manufacturers: ', this.manufacturers );
-      // console.log('data categories: ', this.categories );
-      // console.log('data taxes: ', this.taxes );
-      // console.log('data units: ', this.units );
+      this.attributes = data['attributes_d'];
+      console.log(this.attributes);
     }, error => {
       console.log(error);
-  });
+    });
   }
+  
+  close(){
+    this.viewCtrl.dismiss();
+    this.navCtrl.setRoot(ProductsPage);
+    
+  }
+
+
+  postCombinationProduct(){
+    let body = this.myForm.value;
+    body.product_details = [body.product_details];
+    console.log(body);
+    
+    this.http.post(constants.apipostproduct,
+      body,
+      { headers: new HttpHeaders()
+        .set('authorization', 'http://localhost:4200')
+        .append('app', 'BananaApp')
+        .append('organization', sessionStorage.getItem('organization_id') )
+        .append('user', sessionStorage.getItem('user'))
+        .append('Access-Control-Allow-Origin', '*')
+        .append('token', sessionStorage.getItem('token'))
+      }).subscribe ( data=> {
+        this.product = data['product_id'];
+        this.product_details = data['product_details_ids'];
+        // this.attributes = data['attributes_d'];
+        console.log(this.product);
+        console.log(this.product_details);
+
+        if (this.product != [0]){
+          this.translateService.get('Alerta8').subscribe(
+            value => {
+              let message = value['MensajeAlerta'];
+                const toast = this.toastCtrl.create({
+                message: message,
+                duration: 3000
+                });
+            toast.present();
+          });
+        }
+      }, error => {
+        console.log(error);
+        if (error.ok == false){
+          this.translateService.get('Alerta9').subscribe(
+            value => {
+              let message = value['MensajeAlerta'];
+                const toast = this.toastCtrl.create({
+                message: message,
+                duration: 3000
+                });
+            toast.present();
+          });
+        }
+    });
+  }
+
 
 }
