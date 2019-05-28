69af31e8cc42a24c71f8243905cc6ec5986f6e7a
cskrada <frada@overcom.net>
2019-05-28 15:04:04 -0430

    constantes de todos los servicios actualizados y utilizados para las nuevas funciones

--- src/const/const.ts
+++ src/const/const.ts
@@ -1,20 +1,40 @@
 export const constants = {
-    
+    // Login
     apilogin : 'http://18.223.231.86/laravel-banana/public/api/login',
+
+    // Terceros
     apiclients : 'http://18.223.231.86/laravel-banana/public/api/thirds/customers/',
+    apiseeclient : 'http://18.223.231.86/laravel-banana/public/api/thirds/customer/',
+    apipostclient: 'http://18.223.231.86/laravel-banana/public/api/thirds/prospect/create',
+    apiputclient: 'http://18.223.231.86/laravel-banana/public/api/thirds/prospect/update/',
+
+    // Terceros - contactos|sucursales
+    apicontactsclient: 'http://18.223.231.86/laravel-banana/public/api/thirds/contacts/',
+    apipostcontact: 'http://18.223.231.86/laravel-banana/public/api/thirds/contact/create',
+    apiseecontact: 'http://18.223.231.86/laravel-banana/public/api/contact/',
+    apiputcontact: 'http://18.223.231.86/laravel-banana/public/api/thirds/contact/update',
+    apibranchoffices: 'http://18.223.231.86/laravel-banana/public/api/thirds/branchs/',
+    apiseebranch: 'http://18.223.231.86/laravel-banana/public/api/branch/',
+    apipostbranch: 'http://18.223.231.86/laravel-banana/public/api/thirds/branch/create',
+    apiputbranch: 'http://18.223.231.86/laravel-banana/public/api/thirds/branch/update',
+    apicharges: 'http://18.223.231.86/laravel-banana/public/api/charges/list',
+
+    // Regiones
     apicountries : 'http://18.223.231.86/laravel-banana/public/api/location/countries',
     apistates : 'http://18.223.231.86/laravel-banana/public/api/location/states',
     apicities : 'http://18.223.231.86/laravel-banana/public/api/location/cities',
-    apipostclient: 'http://18.223.231.86/laravel-banana/public/api/thirds/prospect/create',
 
+    // Productos
     apiproducts: 'http://18.223.231.86/laravel-banana/public/api/products',
     apiseeproduct: 'http://18.223.231.86/laravel-banana/public/api/product/',
     apiproducts_details: 'http://18.223.231.86/laravel-banana/public/api/products/details',
     resources: 'http://18.223.231.86/laravel-banana/public/api/products/resources',
     apipostproduct: 'http://18.223.231.86/laravel-banana/public/api/products/create',
 
+    // Organizacion
     apiorganization: 'http://18.223.231.86/laravel-banana/public/api/organizations/byUser',
 
+    // Imagen
     apiimage: 'http://18.223.231.86/laravel-banana/public/storage/img/'
 
     // apilogin: 'http://192.168.1.3:8000/api/login',
